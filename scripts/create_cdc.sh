31ca8e976cd980f85de28a5a1bd5adf317167f05
Tuan Vuong <vanhtuan0409@gmail.com>
2022-09-12 13:38:54 +0700

    update

--- scripts/create_cdc.sh
+++ scripts/create_cdc.sh
@@ -1,9 +1,9 @@
 #!/bin/env bash
 
 here=`cd $(dirname $BASH_SOURCE); pwd`
+CDC_ENDPOINT=${CDC_ENDPOINT:-http://127.0.0.1:8300}
 
-tiup cdc cli changefeed create \
-  --pd="http://127.0.0.1:2379" \
-  --changefeed-id="kafka-changefeed" \
-  --sink-uri="kafka://127.0.0.1:9092/kafka-pinot-cdc?protocol=canal-json" \
-  --config="${here}/../configs/tidb/changefeed.conf"
+curl -v -X POST \
+  -H "Content-Type: application/json" \
+  -d "@${here}/../configs/tidb/changefeed.json" \
+  $CDC_ENDPOINT/api/v2/changefeeds
--- scripts/create_pinot_table.sh
+++ scripts/create_pinot_table.sh
@@ -1,7 +1,14 @@
 #!/bin/env bash
 
-docker exec tidb-pinot-pinot-controller-1 \
-  /opt/pinot/bin/pinot-admin.sh AddTable \
-  -schemaFile=/config/schema.json \
-  -tableConfigFile=/config/table.json \
-  -exec
+here=`cd $(dirname $BASH_SOURCE); pwd`
+CONTROLLER_ENDPOINT=${CONTROLLER_ENDPOINT:-http://127.0.0.1:9000}
+
+curl -v -X POST \
+  -H "Content-Type: application/json" \
+  -d "@${here}/../configs/pinot/schema.json" \
+  $CONTROLLER_ENDPOINT/schemas
+
+curl -v -X POST \
+  -H "Content-Type: application/json" \
+  -d "@${here}/../configs/pinot/table.json" \
+  $CONTROLLER_ENDPOINT/tables
